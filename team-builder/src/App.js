ee12c543922fea3c391e30b28ca2e6a9bc3fd3e8
Justin A <jarata@gmail.com>
2019-12-19 15:51:04 -0800

    Form.js setup inputs, member state, handleFunctions App.js has members state, new member variable deleted unused files basic styling to display members

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import Form from "./components/form/Form";
 
 function App() {
+  const [members, setMembers] = useState([
+    {
+      id: 1,
+      name: "Justin",
+      email: "justin@forms.management",
+      role: "developer"
+    }
+  ]);
+
+  const addMember = (member) => {
+    const newMember = {
+      id: 1,
+      name: member.name,
+      email: member.email,
+      role: member.role
+    };
+    setMembers([...members, newMember])
+  }
+  console.log("App:", members)
   return (
     <div className="App">
-      Test
+      <Form addMember={addMember}/>
+      <div className="members">
+        <h3>Member Output:</h3>
+        {members.map(m =>
+          <ul className="member" key={m}>
+            <p>{m.id}</p>
+            <p>{m.name}</p>
+            <p>{m.email}</p>
+            <p>{m.role}</p>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
